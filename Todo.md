2e515f1a5d52c039361c939524861749bf998ff6
duchieu307 <duchieu307@gmail.com>
2020-09-28 16:26:06 +0700

    Update works 28/9. Check Todo.md for work 29/9

--- Todo.md
+++ Todo.md
@@ -3,7 +3,7 @@ https://stackblitz.com/github/DanielYKPan/ng-alarm?file=src%2Fapp%2Fstore%2Falar
 Công nghệ
 Angular Store + Redux + Effect
 Queue => Background Process để xử lý time 
-Catch Event ?
+Catch Event ? Set Interval (trong reactjs) để tạo luồng chạy ngầm
 
 How to do ?
 Sau khi đã được báo, lưu loging rồi kiểm tra thuộc tính repeat(theo giây, phút, giờ, ngày, ...) để tạo một reminder mới 
@@ -12,7 +12,8 @@ Todo
 1. Phân tích data (có thể nói là xong)
 2. Tạo model và create cơ bản (đang làm cái này)
     Đang tạo create cơ bản
-    Chuyển từ FormGroup sang FormBuilder để tạo mảng các sự kiện
+    Chuyển từ FormGroup sang FormBuilder để tạo mảng các sự kiện (đã chuyển sang FormBuilder)
+    Tạo mảng các thời điểm
     Xử lý lặp trong ngày, tuần, tháng
     Tạo 1 form hoàn chỉnh
 3. Tạo data cơ bản với redux, action là create 
--- src/app/alarm-settings/alarm-settings.component.ts
+++ src/app/alarm-settings/alarm-settings.component.ts
@@ -1,47 +1,57 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, FormBuilder, FormArray } from '@angular/forms';
 
 @Component({
   selector: 'app-alarm-settings',
   templateUrl: './alarm-settings.component.html',
-  styleUrls: ['./alarm-settings.component.css']
+  styleUrls: ['./alarm-settings.component.css'],
 })
 export class AlarmSettingsComponent implements OnInit {
   myTime;
   moments = [
     {
-      name : 'moment1',
-      time : ''
+      name: 'moment1',
+      time: '',
     },
   ];
-  numberOfMoment = 1 ;
+  numberOfMoment = 1;
   showMin: boolean = true;
   showSec: boolean = true;
   repeatUi: boolean = false;
+  alarmForm: FormGroup;
 
-  setAlarmForm = new FormGroup({
-    eventName : new FormControl(''),
-    formTime : new FormControl(new Date()),
-    repeat : new FormControl(),
-    repeatSecond : new FormControl(),
-    repeatHour : new FormControl(),
-    repeatMinute : new FormControl(),
-  });
-
-  constructor() { }
-
-  ngOnInit() {
-    console.log(this.myTime = new Date());
-    this.myTime = new Date();
+  // setAlarmForm = new FormGroup({
+  //   eventName : new FormControl(''),
+  //   formTime : new FormControl(new Date()),
+  //   repeat : new FormControl(),
+  //   repeatSecond : new FormControl(),
+  //   repeatHour : new FormControl(),
+  //   repeatMinute : new FormControl(),
+  // });
+  createAlarmForm() {
+    this.alarmForm = this.formBuilder.group({
+      eventName: [''],
+      repeat: [false],
+      repeatSecond: [0],
+      repeatHour: [0],
+      repeatMinute: [0],
+      formTime: [new Date()],
+    });
   }
 
-  turnOnRepeatUi(){
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit() {
+    console.log((this.myTime = new Date()));
+    this.myTime = new Date();
+    this.createAlarmForm()
+  }
+
+  turnOnRepeatUi() {
     this.repeatUi = true;
   }
 
-  turnOffRepeatUi(){
+  turnOffRepeatUi() {
     this.repeatUi = false;
   }
-
-
 }
